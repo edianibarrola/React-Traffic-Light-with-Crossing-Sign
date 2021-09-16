2ba0675561261747a446136fbe6e980b225f9bcd
edianibarrola <edianibarrola@gmail.com>
2021-09-16 18:35:24 +0000

    genericpush

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,7 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
+    path: path.resolve(__dirname, 'public')
   },
   module: {
     rules: [
